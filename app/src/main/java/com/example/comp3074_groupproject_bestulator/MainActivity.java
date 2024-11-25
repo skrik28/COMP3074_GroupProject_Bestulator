cb8258d656c50f2f7a091c43151a1440efd2a4dd
Simon K <Simon@DESKTOP-R9UCS0H>
2024-11-24 20:47:46 -0800

    init commit

--- app/src/main/java/com/example/comp3074_groupproject_bestulator/MainActivity.java
+++ app/src/main/java/com/example/comp3074_groupproject_bestulator/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -12,6 +15,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.appcompat.widget.Toolbar;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Set a title for the Toolbar
         getSupportActionBar().setTitle("Home");
+
 
 
 //        EdgeToEdge.enable(this);
@@ -47,22 +53,35 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.navigation_home:
-                startActivity(new Intent(MainActivity.this, MainActivity.class));
-                return true;
-            case R.id.navigation_second_activity:
-                startActivity(new Intent(MainActivity.this, SecondActivity.class));
-                return true;
-            case R.id.navigation_third_activity:
-                startActivity(new Intent(MainActivity.this, ThirdActivity.class));
-                return true;
-            case R.id.navigation_fourth_activity:
-                startActivity(new Intent(MainActivity.this, FourthActivity.class));
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.navigation_home) {
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
+            return true;
+        } else if (id == R.id.navigation_second_activity) {
+            startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            return true;
+        } else if (id == R.id.navigation_third_activity) {
+            startActivity(new Intent(MainActivity.this, ThirdActivity.class));
+            return true;
+        } else if (id == R.id.navigation_fourth_activity) {
+            startActivity(new Intent(MainActivity.this, FourthActivity.class));
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void goToDashboard(View v) {
+
+
+        Button goToScreen2 = (Button) v;
+        goToScreen2.setText("changing...");
+
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+        startActivity(intent);
     }
 }
